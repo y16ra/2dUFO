80496a0c88e1322cd1f2b5bff6433b5bd68632ce
y16ra <iyuichi.kny@gmail.com>
2016-06-01 12:50:51 +0900

    Unity Ads対応

---
+++ Assets/Completed/Scripts/UnityAdsButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.Advertisements;
+
+public class UnityAdsButton : MonoBehaviour
+{
+	void Start() {
+		if (Application.platform == RuntimePlatform.Android) {
+			Advertisement.Initialize("1076548");
+			Debug.Log("Android !!!");
+		} else if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			Advertisement.Initialize("1076547");
+			Debug.Log("iOS !!!");
+		} else {
+			Advertisement.Initialize("1076548");
+			Debug.Log("Others !!!");
+		}
+		Debug.Log("start!!!");
+	}
+
+	public void OnClick()
+	{
+		Debug.Log("ready? : " + Advertisement.IsReady());
+		if (Advertisement.IsReady())
+		{
+			Advertisement.Show();
+			Debug.Log("Show!");
+		}
+		Debug.Log("on click End");
+	}
+}
